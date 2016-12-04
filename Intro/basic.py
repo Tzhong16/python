dc132e0aa0a84b54d73dff9b504748b38284bcf9
T.zhong16 <t.zhong16@gmail.com>
2016-12-04 22:57:27 +1300

    updated basic.py

--- Intro/basic.py
+++ Intro/basic.py
@@ -1,2 +1,22 @@
 print(7/8)
 print(5*6)
+
+#Exponentiation
+print(4 ** 2)
+
+#Modulo
+print(18 % 7)
+
+
+print(100 * 1.1 **7)
+
+type()
+
+savings = 100
+
+print(savings)
+
+savings = 100
+factor = 1.1
+result = savings * factor**7
+print(result)
