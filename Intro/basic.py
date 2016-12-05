759346dbd710bf1834dc0f268e2800a84f45edeb
T.zhong16 <t.zhong16@gmail.com>
2016-12-05 13:10:52 +1300

    updated basic.py

--- Intro/basic.py
+++ Intro/basic.py
@@ -33,8 +33,63 @@ year1 = savings * factor
 
 print(type(year1))
 
-# Assign sum of desc and desc to doubledesc
 doubledesc = desc + desc 
 
-# Print out doubledesc
 print(doubledesc)
+
+#create list
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+areas = [hall, kit, liv, bed, bath]
+
+print(areas)
+
+areas = ["hallway", hall, "kitchen", kit, "living room", liv, "bedroom", bed, "bathroom", bath]
+
+print(areas)
+
+house = [["hallway", hall],
+         ["kitchen", kit],
+         ["living room", liv],
+         ["bedroom", bed],
+         ["bathroom", bath]]
+
+print(house)
+print(type(house))
+
+#subsect from a list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+print(areas[1])
+print(areas[-1])
+print(areas[5])
+
+eat_sleep_area = areas[3] + areas[7]
+print(eat_sleep_area)
+
+# substract one to six element 
+downstairs = areas[:6]
+
+upstairs = areas[-4:]
+
+print(downstairs)
+print(upstairs)
+
+#change list value
+areas[-1] = 10.50
+areas[-1] = 10.50
+
+areas_1 = areas + ["poolhouse", 24.5]
+
+areas_2 = areas_1 + ["garage", 15.45]
+
+#creat a copy list that do not affect original list
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+areas_copy = list(areas)
+areas_copy[0] = 5.0
+print(areas)
+
+
