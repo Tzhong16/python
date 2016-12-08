46b6e66ce39cd122a287ddd848633a6cd18d11c7
T.zhong16 <t.zhong16@gmail.com>
2016-12-08 22:03:53 +1300

    updated import_other_file.py

--- import_other_file.py
+++ import_other_file.py
@@ -56,3 +56,110 @@ df2 = xl.parse( 1, parse_cols= [0], skiprows= [0], names= ['Country'])
 print(df2.head())
 
 
+
+
+
+##############################################################
+# SAS file
+###############################################################
+
+from sas7bdat import SAS7BDAT
+
+with SAS7BDAT('sales.sas7bdat') as file:
+     df_sas = file.to_data_frame()
+
+print(df_sas.head())
+
+pd.DataFrame.hist(df_sas[['P']])
+plt.ylabel('count')
+plt.show()
+
+################################################################
+# Stata file 
+###############################################################
+
+import pandas as pd
+
+df = pd.read_stata('disarea.dta')
+
+print(df.head())
+
+pd.DataFrame.hist(df[['disa10']])
+plt.xlabel('Extent of disease')
+plt.ylabel('Number of coutries')
+plt.show()
+
+
+#################################################################
+# HDF5 file
+##################################################################
+
+# HDF5 Store large amount of numeric data
+
+import numpy as np
+import h5py
+
+file = 'LIGO_data.hdf5'
+
+data = h5py.File(file, 'r')
+
+print(type(data))
+
+# Print the keys of the file
+for key in data.keys():
+    print(key)
+
+
+
+
+# Check out keys of group
+group = data['strain']
+for key in group.keys():
+    print(key)
+
+# Set variable equal to time series data: strain
+strain = data['strain']['Strain'].value
+
+# Set number of time points to sample: num_samples
+num_samples = 10000
+
+# Set time vector
+time = np.arange(0, 1, 1/num_samples)
+
+plt.plot(time, strain[:num_samples])
+plt.xlabel('GPS Time (s)')
+plt.ylabel('strain')
+plt.show()
+
+
+######################################################################
+# MATLAP
+#####################################################################
+
+
+import scipy.io
+
+mat = scipy.io.loadmat('albeck_gene_expression.mat')
+
+print(type(mat))
+
+
+import scipy.io
+import matplotlib.pyplot as plt
+import numpy as np
+
+print(mat.keys())
+
+print(type(mat['CYratioCyt']))
+
+print(np.shape(mat['CYratioCyt']))
+
+data = mat['CYratioCyt'][25, 5:]
+fig = plt.figure()
+plt.plot(data)
+plt.xlabel('time (min.)')
+plt.ylabel('normalized fluorescence (measure of expression)')
+plt.show()
+
+
+
