23ca65491670c6c2f6352573f31a3b161800662d
T.zhong16 <t.zhong16@gmail.com>
2016-12-09 23:17:19 +1300

    added py_manipulation.py

--- py_manipulation.py
+++ py_manipulation.py
@@ -1,3 +1,14 @@
-# Intro Database 
+# Connect to  Database 
+# 1. create a engine connect to the file
+# 
+
+from sqlalchemy import create_engine
+
+#the string part consisted of two parts: vendor and path, in this case is sqlite and census.sqlite, respectively
+engine = create_engine('sqlite:///census.sqlite')
+
+print(engine.table_names())
+
+#Reflection is a automatic  process of reading the database and building the metadata based on that information
 
 
