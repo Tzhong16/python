ed7466e96f92fe96cf9fb1190f2351f5b61d2e08
T.zhong16 <t.zhong16@gmail.com>
2016-12-12 12:19:00 +1300

    added advanced SQLAlchemy

--- py_manipulation.py
+++ py_manipulation.py
@@ -217,3 +217,92 @@ results = connection.execute(stmt).fetchall()
 print(results)
 
 print(results[0].keys())
+
+
+
+#feed a ResultProxy directly into a pandas DataFrame
+import pandas as pd
+
+df = pd.DataFrame(results)
+
+# Set column names
+df.columns = results[0].keys()
+
+print(df)
+
+
+
+# Import Pyplot as plt from matplotlib
+from  matplotlib import pyplot as plt 
+
+# Create a DataFrame from the results: df
+df = pd.DataFrame(results)
+
+# Set Column names
+df.columns = results[0].keys()
+
+# Print the DataFrame
+print(df)
+
+# Plot the DataFrame
+df.plot.bar()
+plt.show()
+
+
+
+
+####################################
+#Advanced SQLAlchemy query
+####################################
+
+from sqlalchemy import create_engine
+
+# Create an engine to the census database
+# dialect and driver:   mysql+pymysql://   ;   username:password : student:datacamp  ; host and port : @courses.csrrinzqubik.us-east-1.rds.amazonaws.com:3306/  ; database_name: census
+# dialect+driver://username:password@host:port/database_name
+ 
+engine = create_engine('mysql+pymysql://student:datacamp@courses.csrrinzqubik.us-east-1.rds.amazonaws.com:3306/census')
+
+print(engine.table_names())
+
+
+# Build query to return state names by population difference from 2008 to 2000: stmt
+stmt = select([census.columns.state, (census.columns.pop2008-census.columns.pop2000).label('pop_change')])
+
+stmt = stmt.group_by(census.columns.state)
+
+# Append order by for pop_change descendingly: stmt
+stmt = stmt.order_by(desc('pop_change'))
+
+stmt = stmt.limit(5)
+
+results = connection.execute(stmt).fetchall()
+
+# Print the state and population change for each record
+for result in results:
+    print('{}-{}'.format(result.state, result.pop_change))
+
+
+
+#Determining the Overall Percentage of Females
+
+
+from sqlalchemy import case, cast, Float
+
+# Build an expression to calculate female population in 2000
+female_pop2000 = func.sum(
+    case([
+        (census.columns.sex == 'F', census.columns.pop2000)
+    ], else_=0))
+
+# Cast an expression to calculate total population in 2000 to Float
+total_pop2000 = cast(func.sum(census.columns.pop2000), Float)
+
+# Build a query to calculate the percentage of females in 2000: stmt
+stmt = select([female_pop2000 / total_pop2000* 100])
+
+# Execute the query and store the scalar result: percent_female
+percent_female = connection.execute(stmt).scalar()
+
+# Print the percentage
+print(percent_female)
