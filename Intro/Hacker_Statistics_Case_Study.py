0d2123547b520937c9d75a708658f91025c35d14
T.zhong16 <t.zhong16@gmail.com>
2016-12-13 23:17:05 +1300

    added Roll dice steps

--- Intro/Hacker_Statistics_Case_Study.py
+++ Intro/Hacker_Statistics_Case_Study.py
@@ -1,3 +1,53 @@
 #Case study: Hacker Statistics
 
+# random package is a sub-package in numpy
+
+
+##########################
+#random float
+##########################
+
+# seed()   rand()
+
+import numpy as np
+
+np.random.seed(123)
+
+print(np.random.rand())
+
+
+# randint()  :generate integers randomly
+# Roll the dice
+
+import numpy as np
+np.random.seed(123)
+
+print(np.random.randint(1,7))
+
+# Use randint() again
+print(np.random.randint(1,7))
+
+
+
+# Use if -- elif -- else to determine the next moved
+import numpy as np
+np.random.seed(123)
+
+# Starting step
+step = 50
+
+# Roll the dice
+dice = np.random.randint(1,7)
+
+if dice <= 2 :
+    step = step - 1
+elif dice <= 5 :
+    step = step + 1
+else :
+    step = step + np.random.randint(1,7)
+
+print(dice)
+print(step)
+
+
 
