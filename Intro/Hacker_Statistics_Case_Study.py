f83ae0692dbd33f0c445c160343642e4dd6d3281
T.zhong16 <t.zhong16@gmail.com>
2016-12-14 00:00:08 +1300

    added simulate multiples walks

--- Intro/Hacker_Statistics_Case_Study.py
+++ Intro/Hacker_Statistics_Case_Study.py
@@ -110,3 +110,37 @@ import matplotlib.pyplot as plt
 plt.plot(random_walk)
 
 plt.show()
+
+
+
+##########################
+#simulate multiples walks
+##########################
+
+import numpy as np
+np.random.seed(123)
+
+all_walks = []
+
+for i in range(10) :
+
+    random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
+
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+        random_walk.append(step)
+
+    # Append random_walk to all_walks
+    all_walks.append(random_walk)
+
+print(all_walks)
+
+
+
