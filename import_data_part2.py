ebd9eb9e18c893e70180c44264a79c8390e36e7f
T.zhong16 <t.zhong16@gmail.com>
2016-12-16 12:40:25 +1300

    added API & JSONs importing data

--- import_data_part2.py
+++ import_data_part2.py
@@ -166,7 +166,7 @@ print(guido_title)
 # Get Guido's text: guido_text
 guido_text = soup.get_text()
 
-print(guido_text
+print(guido_text)
 
 
 
@@ -191,4 +191,94 @@ a_tags = soup.find_all('a')
 
 for link in a_tags:
     print(link.get('href'))
-)
+
+
+
+
+#########################################################
+# API   &    JSONs  
+#########################################################
+
+
+# Load JSON: json_data
+with open("a_movie.json") as json_file:
+    json_data = json.load(json_file)
+
+# Print each key-value pair in json_data
+for k in json_data.keys():
+     #access a value in a dictionary using the syntax: dictionary[key]
+    print(k + ': ', json_data[k])
+
+
+
+
+
+
+###############################
+#pulling data from API  
+##############################
+
+
+import requests
+
+url = 'http://www.omdbapi.com/?t=social+network'
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+print(r.text)
+
+
+
+
+#example 2 
+
+
+import requests
+
+url = 'http://www.omdbapi.com/?t=social+network'
+
+r = requests.get(url)
+
+# Decode the JSON data into a dictionary: json_data
+json_data = r.json()
+
+# Print each key-value pair in json_data
+for k in json_data.keys():
+    print(k + ': ', json_data[k])
+
+
+# example 3 
+
+import requests
+
+url = 'https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=pizza'
+
+r = requests.get(url)
+
+# Decode the JSON data into a dictionary: json_data
+json_data = r.json()
+
+# Print the Wikipedia page extract
+pizza_extract = json_data['query']['pages']['24768']['extract']
+print(pizza_extract)
+
+
+
+#####################################################
+#Mock authentication  credentials  
+#####################################################
+
+
+import tweepy
+
+
+access_token = "1092294848-aHN7DcRP9B4VMTQIhwqOYiB14YkW92fFO8k8EPy"
+access_token_secret = "X4dHmhPfaksHcQ7SCbmZa2oYBBVSD2g8uIHXsp5CTaksx"
+consumer_key = "nZ6EA0FxZ293SxGNg8g8aP0HM"
+consumer_secret = "fJGEodwe3KiKUnsYJC3VRndj7jevVvXbK2D5EiJ2nehafRgA6i"
+
+# Pass OAuth details to tweepy's OAuth handler
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
