8690a40eeb0b545268a69187d6d0e4fe01c4a650
T.zhong16 <t.zhong16@gmail.com>
2016-12-15 13:19:10 +1300

    added fecthing HTML by BeautifulSoup

--- import_data_part2.py
+++ import_data_part2.py
@@ -55,6 +55,11 @@ print(xl.keys())
 print(xl['1700'].head())
 
 
+##################################################################
+# Scrap the web, extract the structured date from HTML
+#################################################################
+
+
 ###########
 # http request 
 ###########
@@ -113,4 +118,77 @@ text = r.text
 print(text)
 
 
+#############################
+#BeautifulSoup 
+#############################
 
+# parse and extract structured data from HTML
+
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.python.org/~guido/'
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Extracts the response as html: html_doc
+html_doc = r.text
+
+# Create a BeautifulSoup object from the HTML: soup
+soup = BeautifulSoup(html_doc)
+
+# Prettify the BeautifulSoup object: pretty_soup
+pretty_soup = soup.prettify()
+
+print(pretty_soup
+
+
+
+##example 2 : Getting the text
+
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.python.org/~guido/'
+
+r = requests.get(url)
+
+html_doc = r.text
+
+soup = BeautifulSoup(html_doc)
+
+# Get the title of Guido's webpage: guido_title
+guido_title = soup.title
+
+print(guido_title)
+
+# Get Guido's text: guido_text
+guido_text = soup.get_text()
+
+print(guido_text
+
+
+
+#example 3 : getting the hyperlinks
+
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.python.org/~guido/'
+
+r = requests.get(url)
+
+html_doc = r.text
+
+soup = BeautifulSoup(html_doc)
+
+# Print the title of Guido's webpage
+print(soup.title)
+
+# Find all 'a' tags (which define hyperlinks): a_tags
+a_tags = soup.find_all('a')
+
+for link in a_tags:
+    print(link.get('href'))
+)
