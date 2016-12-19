a9910cdd7a315f4fc48436416e06750bcca03ea8
T.zhong16 <t.zhong16@gmail.com>
2016-12-19 23:12:44 +1300

    updated bokeh.py

--- bokeh.py
+++ bokeh.py
@@ -20,3 +20,26 @@ output_file('fert_lit.html')
 
 show(p)
 
+
+
+#############################
+#Multiple plot in one graph
+############################
+
+# Create the figure: p
+p = figure(x_axis_label='fertility', y_axis_label='female_literacy (% population)')
+
+# Add a circle glyph to the figure p
+p.circle(fertility_latinamerica, female_literacy_latinamerica)
+
+# Add an x glyph to the figure p
+p.x(fertility_africa, female_literacy_africa)
+
+# Specify the name of the file
+output_file('fert_lit_separate.html')
+
+# Display the plot
+show(p)
+
+
+
