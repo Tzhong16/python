9c551af75a0b6eb7cdf2c070496ba9df836ce323
T.zhong16 <t.zhong16@gmail.com>
2016-12-20 22:19:09 +1300

    updated bokeh.py

--- bokeh.py
+++ bokeh.py
@@ -169,3 +169,22 @@ show(p)
 
 
 
+####################################
+#The Bokeh ColumnDataSource 
+####################################
+
+# Import the ColumnDataSource class from bokeh.plotting
+from bokeh.plotting import ColumnDataSource
+
+# Create a ColumnDataSource: source
+source = ColumnDataSource(df)
+
+# Add circle glyphs to the figure p
+p.circle(x='Year', y='Time', color='color', size=8, source=source)
+
+# Specify the name of the output file and show the result
+output_file('sprint.html')
+show(p)
+
+
+
