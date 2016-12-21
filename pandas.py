32c0abfcad3a44284b8ac48888e6e56c83130a6b
T.zhong16 <t.zhong16@gmail.com>
2016-12-21 16:37:45 +1300

    added basic of pandas dataframe

--- pandas.py
+++ pandas.py
@@ -27,3 +27,29 @@ df_log10 = np.log10(df)
 print(type(np_vals), type(np_vals_log10))
 print(type(df), type(df_log10))
 
+
+
+########################################
+#zip list to build a dataframe
+######################################
+
+# Zip the 2 lists together into one list of (key,value) tuples: zipped
+zipped = list(zip(list_keys, list_values))
+
+print(zipped)
+
+# Build a dictionary with the zipped list: data
+data = dict(zipped)
+
+# Build and inspect a DataFrame from the dictionary: df
+df = pd.DataFrame(data)
+print(df)
+
+
+
+# change the label of the dataframe
+
+list_labels = ['year', 'artist', 'song', 'chart weeks']
+
+df.columns = list_labels
+
