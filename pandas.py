68076827715934282b08a543c8719018b8169161
T.zhong16 <t.zhong16@gmail.com>
2016-12-22 23:32:06 +1300

    updated pandas.py

--- pandas.py
+++ pandas.py
@@ -53,3 +53,24 @@ list_labels = ['year', 'artist', 'song', 'chart weeks']
 
 df.columns = list_labels
 
+
+
+
+
+
+
+
+
+
+# Make a string with the value 'PA': state
+state = 'PA'
+
+# Construct a dictionary: data
+data = {'state':state, 'city':cities}
+
+# Construct a DataFrame from dictionary data: df
+df = pd.DataFrame(data)
+
+# Print the DataFrame
+print(df)
+
