d39cf134153130a226fe2182c0e8f7d37fb0c687
T.zhong16 <t.zhong16@gmail.com>
2016-12-23 01:18:13 +1300

    updated pandas.py

--- pandas.py
+++ pandas.py
@@ -74,3 +74,26 @@ df = pd.DataFrame(data)
 # Print the DataFrame
 print(df)
 
+
+
+
+
+
+
+# Create a plot with color='red'
+df.plot(color='red')
+
+# Add a title
+plt.title('Temperature in Austin')
+
+# Specify the x-axis label
+plt.xlabel('Hours since midnight August 1, 2010')
+
+# Specify the y-axis label
+plt.ylabel('Temperature (degrees F)')
+
+# Display the plot
+plt.show()
+
+
+
